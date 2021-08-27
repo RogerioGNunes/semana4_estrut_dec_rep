6a21cd251a7f63be9ba13a9e7109190ebb649a81
RogerioGNunes <rg.nunes2013@gmail.com>
2021-08-27 00:23:45 +0000

    exercício semana 4

---
+++ ExerFor4.java
@@ -0,0 +1,17 @@
+public class ExerFor4 {
+
+    //4. Imprima os fatoriais de 1 a 10.
+
+    public void fatorial(){
+        //5*4*3*2*1
+
+        int fat=5;
+        for(int n=5;n>=1;n=n-1){
+            fat=fat*n;
+        }
+        System.out.println(fat);
+    }
+
+
+    
+}
---
+++ ExerFor5.java
@@ -0,0 +1,16 @@
+public class ExerFor5 {
+    public void par_impar(){
+
+
+    int x=58;
+    if(x%2==0){
+        x = x / 2;
+        System.out.println(x);
+    }
+    else {
+        x=3*x+1;
+        System.out.println(x);
+    }
+    
+    }
+}
